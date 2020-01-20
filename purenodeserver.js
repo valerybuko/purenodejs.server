c9298f2c002e2d5781403b3a20c4c91ae90582d9
valery.buka <valpal314@gmail.com>
2020-01-20 19:33:01 +0300

    add pathname conditions

--- purenodeserver.js
+++ purenodeserver.js
@@ -1,24 +1,44 @@
 const http = require('http');
 const url = require('url');
 
-const PORT = 3000;
-
 function handler(req, res) {
+    const parsedUrl = url.parse(req.url, true);
 
-    const parseUrl = url.parse(req.url, true);
+    res.setHeader('x-server-date', new Date());
 
-    if(parseUrl.pathname === '/') {
-        res.writeHead(200, {'Content-type':'text-plain'});
-        res.write('Webserver has been created');
-        res.end();
+    if(parsedUrl.pathname === '/') {
+        res.writeHead(200, {'Content-type':'text/plain'});
+        res.write('Hello, I am a webserver!');
+        return res.end();
+    } else if(parsedUrl.pathname === '/time') {
+        res.writeHead(200, {'Content-type':'text/plain'});
+        res.write(new Date().toString());
+        return res.end();
+    } else if(parsedUrl.pathname === '/hello') {
+        const name = parsedUrl.query.name;
+        if(!name) {
+            res.writeHead(400, {'Content-type':'text/plain'});
+            return res.end();  
+        }
+        res.writeHead(200, {'Content-type':'text/plain'});
+        res.write(`Hello ${name}`);
+        return res.end();
+    } else if(parsedUrl.pathname.startsWith('/user/')) {
+        const regex = new RegExp('\/user\/(.+)');
+        const matches = regex.exec(parsedUrl.pathname);
+        if(!matches || !matches[1]) {
+            res.writeHead(400, {'Content-type':'text/plain'});
+            return res.end();  
+        }
+        res.writeHead(200, {'Content-type':'text/plain'});
+        res.write(`Userpofile of ${matches[1]}`);
+        return res.end();
     } else {
-        res.writeHead(404, {'Content-type':'text-plain'});
-        res.end();
+        res.writeHead(404, {'Content-type':'text/plain'});
+        return res.end();
     }
-
-    
 }
 
 const server = http.createServer(handler);
 
-server.listen(PORT);
+server.listen(3000);
